2054dfb4ccdb74aa3779f7f30d5c47cfebd8d7ef
Toys0125 <marcus.g.stange@gmail.com>
2023-10-11 10:31:44 -0500

    moved supabase initial to main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createClient } from '@supabase/supabase-js'
-const supabase = createClient('https://xfpbhkpdcvwytubbvjdp.supabase.co', 'public-anon-key')
+const supabase = createClient('https://xfpbhkpdcvwytubbvjdp.supabase.co', import.meta.env.APIKEY)
 
 const app = createApp(App)
 
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { createClient } from '@supabase/supabase-js'
-const supabase = createClient('https://xfpbhkpdcvwytubbvjdp.supabase.co', import.meta.env.APIKEY)
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
