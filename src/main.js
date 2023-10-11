c1fd6e0fc6a9e120ca2aa4e8ff20e0c8d57667e5
Toys0125 <marcus.g.stange@gmail.com>
2023-10-11 10:08:16 -0500

    add dotenv

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createClient } from '@supabase/supabase-js'
+const supabase = createClient('https://xfpbhkpdcvwytubbvjdp.supabase.co', 'public-anon-key')
 
 const app = createApp(App)
 
