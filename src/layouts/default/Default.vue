31ed31a37a88d96d7cd3429836d374d82963474c
Toys0125 <marcus.g.stange@gmail.com>
2023-10-16 11:06:13 -0500

    Add Test page navigation

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -3,7 +3,7 @@
     <v-navigation-drawer v-model="drawer">
       <v-list-item title="Homepage" link></v-list-item>
       <v-divider></v-divider>
-      <v-list-item link title="List Item 1"></v-list-item>
+      <v-list-item to="/test" title="Test Page Nav"></v-list-item>
       <v-list-item link title="List Item 2"></v-list-item>
       <v-list-item link title="List Item 3"></v-list-item>
     </v-navigation-drawer>
@@ -15,39 +15,13 @@
     </v-app-bar>
 
     <v-main>
-      <sorted-table :values="values">
-        <thead>
-          <tr>
-            <th scope="col" style="text-align: left; width: 10rem;">
-              <sort-link name="id">Last</sort-link>
-            </th>
-            <th scope="col" style="text-align: left; width: 10rem;">
-              <sort-link name="id">First</sort-link>
-            </th>
-            <th scope="col" style="text-align: left; width: 10rem;">
-              <sort-link name="id">Student Id</sort-link>
-            </th>
-            <th scope="col" style="text-align: left; width: 10rem;">
-              <sort-link name="id">Presence</sort-link>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="values in database" :key=""><!--need variable names from database-->
-            <td>{{ last }}</td>
-            <td>{{ first }}</td>
-            <td>{{ id }}</td>
-            <td>{{ presence }}</td> <!--should be the dropdown menu-->
-          </tr>
-        </tbody>
-      </sorted-table>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-
 const drawer = ref(null)
 </script>
 
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,11 +1,11 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
-      <h1>Tests</h1>
+      <h1>We switched pages</h1>
     </v-responsive>
   </v-container>
 </template>
 
 <script setup>
-  //
 </script>
