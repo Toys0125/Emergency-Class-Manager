e3e4fb4bec9c4fbf63a19366111a8fdb1893a09b
Toys0125 <marcus.g.stange@gmail.com>
2023-10-18 12:45:22 -0500

    Added Snack bar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <router-view />
+  <snackbar ref="snackbar"/>
 </template>
 
 <script setup>
-  //
+  import snackbar from './components/snackbar.vue';
+</script>
+<script>
+export default {
+  components:{
+    snackbar
+  },
+  mounted() {
+    this.$root.snackbar = this.$refs.snackbar
+    console.log(this.$root.snackbar)
+  }
+}
 </script>
---
+++ src/components/snackbar.vue
@@ -0,0 +1,46 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <v-snackbar
+    v-model="snackBar"
+    :color="snackBarColor"
+    :multi-line="mode === 'multi-line'"
+    :timeout="snackBarTimeout"
+    :vertical="mode === 'vertical'"
+    >{{ snackBarText }}</v-snackbar
+  >
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'snackbar',
+  data: () => ({
+    snackbarColor: 'green',
+    snackBarText: 'Success!',
+    snackBar: false,
+    snackBarColor: 'green',
+    snackBarTimeout: 2000,
+    defaultSnackBarTimeout: 2000,
+    defaultSuccessSnackBarText: 'Success!',
+    defaultSuccessSnackBarColor: 'green',
+    mode: false
+  }),
+  methods: {
+    show(snackbarOptions) {
+      this.snackBarColor =
+        snackbarOptions.color === undefined ? this.defaultSuccessSnackBarColor : snackbarOptions.color
+
+      this.snackBarText =
+        snackbarOptions.text === undefined ? this.defaultSuccessSnackBarText : snackbarOptions.text
+      this.snackBarTimeout = parseInt(
+        snackbarOptions.timeout === undefined || snackbarOptions.timeout === 0
+          ? this.defaultSnackBarTimeout
+          : snackbarOptions.timeout
+      )
+      this.mode = snackbarOptions.mode
+      this.snackBar = true
+      console.log('Snack bar show')
+    }
+  }
+}
+</script>
