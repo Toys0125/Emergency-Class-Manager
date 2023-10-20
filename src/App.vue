c1ed2129a7d9120bb26ff313e44bf9bac34c2620
Toys0125 <marcus.g.stange@gmail.com>
2023-10-20 10:35:15 -0500

    Add Suppabase Login

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,51 @@
 <template>
-  <router-view />
-  <snackbar ref="snackbar"/>
+  <router-view v-if="session" :session="session" />
+  <Auth v-else />
+  <snackbar ref="snackbar" />
 </template>
 
 <script setup>
-  import snackbar from './components/snackbar.vue';
+import snackbar from './components/snackbar.vue'
+import Auth from './components/Auth.vue'
+import supabase from './supabase'
+import { ref } from 'vue'
+import { onMounted } from 'vue'
+const session = ref()
+
+onMounted(() => {
+  supabase.auth.getSession().then(({ data }) => {
+    session.value = data.session
+  })
+
+  supabase.auth.onAuthStateChange((_, _session) => {
+    session.value = _session
+  })
+})
 </script>
 <script>
 export default {
-  components:{
-    snackbar
+  components: {
+    snackbar,
+    Auth
   },
   mounted() {
     this.$root.snackbar = this.$refs.snackbar
     console.log(this.$root.snackbar)
+  },
+  computed:{
+    async checkSession() {
+      const { data, error } = await supabase.auth.getSession()
+      if (error) {
+        this.$root.snackbar.show({ text: 'Auth Error', timeout: 5000, color: 'red' })
+        return false
+      }
+      if (data.session == null) return false
+      console.log(data.session)
+      return true
+    }
+  },
+  methods: {
+
   }
 }
 </script>
