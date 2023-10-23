1d93f91c2b5dd9af9de6ad01660f7e68916f9559
Toys0125 <marcus.g.stange@gmail.com>
2023-10-23 15:22:36 -0500

    current work on users

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,115 +1,39 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
+  <v-responsive class="fill-width">
+    <v-text-field v-model="search" placeholder="Search Here" :onchange="searchRows"></v-text-field>
+  </v-responsive>
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
       <v-data-table-server
-        v-model:items-per-page="itemsPerPage"
+        v-model:items-per-page="rowsPerPage"
         :headers="headers"
-        :items-length="totalItems"
-        :items="serverItems"
+        :items-length="totalrows"
+        :items="rows"
         :loading="loading"
         class="elevation-1"
         item-value="name"
-        @update:options="loadItems"
-      ></v-data-table-server>
+        @update:options="loadRows"
+      >
+        <template v-slot:item.admin="{ value }"><v-checkbox prepend-icon="verified_user" :model-value="value"></v-checkbox></template>
+      </v-data-table-server>
     </v-responsive>
   </v-container>
 </template>
 
 <script setup>
+import supabase from '@/supabase'
 import { VDataTableServer } from 'vuetify/lib/labs/components.mjs'
 </script>
 <script>
-const desserts = [
-  {
-    name: 'Frozen Yogurt',
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    iron: '1'
-  },
-  {
-    name: 'Jelly bean',
-    calories: 375,
-    fat: 0.0,
-    carbs: 94,
-    protein: 0.0,
-    iron: '0'
-  },
-  {
-    name: 'KitKat',
-    calories: 518,
-    fat: 26.0,
-    carbs: 65,
-    protein: 7,
-    iron: '6'
-  },
-  {
-    name: 'Eclair',
-    calories: 262,
-    fat: 16.0,
-    carbs: 23,
-    protein: 6.0,
-    iron: '7'
-  },
-  {
-    name: 'Gingerbread',
-    calories: 356,
-    fat: 16.0,
-    carbs: 49,
-    protein: 3.9,
-    iron: '16'
-  },
-  {
-    name: 'Ice cream sandwich',
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-    protein: 4.3,
-    iron: '1'
-  },
-  {
-    name: 'Lollipop',
-    calories: 392,
-    fat: 0.2,
-    carbs: 98,
-    protein: 0,
-    iron: '2'
-  },
-  {
-    name: 'Cupcake',
-    calories: 305,
-    fat: 3.7,
-    carbs: 67,
-    protein: 4.3,
-    iron: '8'
-  },
-  {
-    name: 'Honeycomb',
-    calories: 408,
-    fat: 3.2,
-    carbs: 87,
-    protein: 6.5,
-    iron: '45'
-  },
-  {
-    name: 'Donut',
-    calories: 452,
-    fat: 25.0,
-    carbs: 51,
-    protein: 4.9,
-    iron: '22'
-  }
-]
-
+/*
 const FakeAPI = {
   async fetch({ page, itemsPerPage, sortBy }) {
     return new Promise((resolve) => {
       setTimeout(() => {
         const start = (page - 1) * itemsPerPage
         const end = start + itemsPerPage
-        const items = desserts.slice()
+        //const items = desserts.slice()
 
         if (sortBy.length) {
           const sortKey = sortBy[0].key
@@ -127,38 +51,114 @@ const FakeAPI = {
       }, 500)
     })
   }
+}*/
+const supabaseRetrive = {
+  async count() {
+    const { count, error } = await supabase
+      .from('Users')
+      .select('*', { count: 'exact', head: true })
+    if (error) {
+      console.error(error)
+      this.$root.snackbar.show({ text: 'Error check log', timeout: 10000, color: 'red' })
+    }
+    console.log(count)
+    return count
+  },
+  async fetch({ page = 0, itemsPerPage = 50, sortBy = 'desc' }) {
+    var from = (page - 1) * itemsPerPage
+    var to = page * itemsPerPage
+    console.log(from, to)
+    const { data, error } = await supabase
+      .from('Users')
+      .select('*')
+      .range(from, to)
+      .neq('removed', true)
+    console.log(data)
+    if (error) {
+      console.error(error)
+      this.$root.snackbar.show({ text: 'Error check log', timeout: 10000, color: 'red' })
+    }
+    if (sortBy.length) {
+      const sortKey = sortBy[0].key
+      const sortOrder = sortBy[0].order
+      data.sort((a, b) => {
+        const aValue = a[sortKey]
+        const bValue = b[sortKey]
+        return sortOrder === 'desc' ? bValue - aValue : aValue - bValue
+      })
+    }
+    return { rows: data }
+  },
+  async search({ page = 0, itemsPerPage = 50, sortBy = 'desc', text = '' }) {
+    var from = (page - 1) * itemsPerPage
+    var to = page * itemsPerPage
+    console.log(from, to)
+    const { data, error } = await supabase.rpc('searchusers', text).range(from, to)
+    console.log(data)
+    if (error) {
+      console.error(error)
+      this.$root.snackbar.show({ text: 'Error check log', timeout: 10000, color: 'red' })
+    }
+    if (sortBy.length) {
+      const sortKey = sortBy[0].key
+      const sortOrder = sortBy[0].order
+      data.sort((a, b) => {
+        const aValue = a[sortKey]
+        const bValue = b[sortKey]
+        return sortOrder === 'desc' ? bValue - aValue : aValue - bValue
+      })
+    }
+    return { rows: data }
+  }
 }
 
 export default {
   data: () => ({
-    itemsPerPage: 5,
+    rowsPerPage: 5,
     headers: [
       {
-        title: 'Dessert (100g serving)',
+        title: 'userEmail',
         align: 'start',
         sortable: false,
-        key: 'name'
+        key: 'userEmail'
       },
-      { title: 'Calories', key: 'calories', align: 'end' },
-      { title: 'Fat (g)', key: 'fat', align: 'end' },
-      { title: 'Carbs (g)', key: 'carbs', align: 'end' },
-      { title: 'Protein (g)', key: 'protein', align: 'end' },
-      { title: 'Iron (%)', key: 'iron', align: 'end' }
+      { title: 'First Name', key: 'fName', align: 'end' },
+      { title: 'Last Name', key: 'lName', align: 'end' },
+      { title: 'admin', key: 'admin', align: 'end' }
     ],
-    serverItems: [],
+    rows: [],
     loading: true,
-    totalItems: 0
+    totalrows: 0,
+    search: '',
+    options: { page: 1, rowsPerPage: 5, sortBy: {} }
   }),
   methods: {
-    loadItems({ page, itemsPerPage, sortBy }) {
+    loadRows({ page, rowsPerPage, sortBy }) {
       this.loading = true
-      FakeAPI.fetch({ page, itemsPerPage, sortBy }).then(({ items, total }) => {
-        this.serverItems = items
-        this.totalItems = total
-        this.loading = false
-        this.$root.snackbar.show({ text: 'Loaded', timeout:2000,color:"blue"})
-      })
+      if (this.totalrows == 0) {
+        this.toalrows = supabaseRetrive.count()
+      }
+      this.options = { page: page, rowsPerPage: rowsPerPage, sortBy: sortBy }
+      if (this.search.length < 3) {
+        supabaseRetrive.fetch({ page, rowsPerPage, sortBy }).then(({ rows }) => {
+          this.rows = rows
+          this.loading = false
+          this.$root.snackbar.show({ text: 'Loaded', timeout: 2000, color: 'blue' })
+        })
+      } else {
+        this.searchRows()
+      }
+      console.log(this.$root.snackbar)
     },
+    searchRows() {
+      if (this.search.length < 3) return
+      supabaseRetrive.search({
+        page: this.options.page,
+        rowsPerPage: this.options.rowsPerPage,
+        sortBy: this.options.sortBy,
+        text: this.search
+      })
+    }
   }
 }
 </script>
