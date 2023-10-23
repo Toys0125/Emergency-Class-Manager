f69552cd21958c16f3ed05ea8178469c17fe0f4c
TPeacockAPSU <79169949+TPeacockAPSU@users.noreply.github.com>
2023-10-23 11:26:27 -0500

    Update Default.vue

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -16,23 +16,112 @@
     </v-app-bar>
 
     <v-main>
-      <v-container>
-        <router-view />
-      </v-container>
+      <v-container class="fill-height">
+      <v-responsive class="align-center text-center fill-height">
+        <v-data-table-server
+          v-model:items-per-page="itemsPerPage"
+          :headers="headers"
+          :items-length="totalItems"
+          :items="serverItems"
+          :loading="loading"
+          class="elevation-1"
+          item-value="name"
+          @update:options="loadItems"
+        ></v-data-table-server>
+      </v-responsive>
+    </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-/*import { ref } from 'vue'
-const drawer = ref(null)*/
+  import { VDataTableServer } from 'vuetify/lib/labs/components.mjs'
 </script>
 
 <script>
-/*import EventBus, { ACTIONS } from '@/EventBus/index';*/
-export default {
-  data: () => ({
-    drawer: false
-  })
-}
-</script>
+const desserts = [
+    {
+      StudentID: 'Frozen Yogurt',
+      lName: 'hi',
+      fName: 'hi',
+    },
+    {
+      StudentID: 'Jelly bean',
+      lName: 'hi',
+      fName: 'hi',
+    },
+    {
+      StudentID: 'KitKat',
+      lName: 'hi',
+      fName: 'hi',
+    },
+    {
+      StudentID: 'Eclair',
+      lName: 'hi',
+      fName: 'hi'
+    },
+    {
+      StudentID: 'Gingerbread',
+      lName: 'hi',
+      fName: 'hi',
+    },
+    {
+      StudentID: 'Ice cream sandwich',
+      lName: 'hi',
+      fName: 'hi',
+    },
+  ]
+  
+  const FakeAPI = {
+    async fetch({ page, itemsPerPage, sortBy }) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const start = (page - 1) * itemsPerPage
+          const end = start + itemsPerPage
+          const items = desserts.slice()
+  
+          if (sortBy.length) {
+            const sortKey = sortBy[0].key
+            const sortOrder = sortBy[0].order
+            items.sort((a, b) => {
+              const aValue = a[sortKey]
+              const bValue = b[sortKey]
+              return sortOrder === 'desc' ? bValue - aValue : aValue - bValue
+            })
+          }
+  
+          const paginated = items.slice(start, end)
+  
+          resolve({ items: paginated, total: items.length })
+        }, 500)
+      })
+    }
+  }
+  
+  export default {
+    data: () => ({
+      itemsPerPage: 5,
+      headers: [
+        { title: 'Student ID', key: 'StudentID', align: 'end' },
+        { title: 'Last Name', key: 'lName',  align: 'end' },
+        { title: 'First Name', key: 'fName', align: 'end' },
+        { title: 'Presence', key: 'presence', align: 'end'}
+      ],
+      serverItems: [],
+      loading: true,
+      totalItems: 0
+    }),
+    methods: {
+      loadItems({ page, itemsPerPage, sortBy }) {
+        this.loading = true
+        FakeAPI.fetch({ page, itemsPerPage, sortBy }).then(({ items, total }) => {
+          this.serverItems = items
+          this.totalItems = total
+          this.loading = false
+          this.$root.snackbar.show({ text: 'Loaded', timeout:2000,color:"blue"})
+        })
+      },
+    }
+  }
+  </script>
+
