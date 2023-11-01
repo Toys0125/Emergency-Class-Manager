a410681488bd858dcb2ec6c276a87801200447d0
khoskins2 <hoskinskatie1@gmail.com>
2023-11-01 13:00:09 -0500

    Need dates fixed when retrieving from DB

--- src/views/In Pages/Events.vue
+++ src/views/In Pages/Events.vue
@@ -10,7 +10,12 @@
 
     <div>
       <label for="eventDate">Event Date:</label>
-      <input type="datetime-local" id="eventDate" v-model="newEventDate" />
+      <input type="date" id="eventDate" v-model="newEventDate" />
+    </div>
+
+    <div>
+      <label for="eventTime">Event Time:</label>
+      <input type="time" id="scheduledTime" v-model="newEventTime" />
     </div>
 
     <div>
@@ -40,56 +45,85 @@ export default {
       calendarOptions: {
         plugins: [dayGridPlugin],
         initialView: 'dayGridMonth',
-        weekends: true
+        weekends: false,
+        events: []
       },
       newEventTitle: '',
       newEventDate: '',
-      newEventDesc: ''
+      newEventTime: '',
+      newEventDesc: '',
+    };
+  },
+  async mounted() {
+    // Load events from the database and populate the calendar
+    const { data: events, error } = await supabase
+      .from('Events')
+      .select('*');
+
+    console.log(events)
+    
+    if (error) {
+      console.error(error);
+    } else {
+      // Populate events in the calendar
+      this.calendarOptions.events = events.map(event => ({
+        title: event.eventName,
+        //It does not like this .-.
+        //start: new Date(event.date),
+        //start: new Date(event.date + 'T' + event.scheduledTime),
+        //start: new Date(event.date + event.scheduledTime),
+        //start: new Date(`${event.date} + 'T' + ${event.scheduledTime}`)
+        //start: new Date(`${event.date} + ${event.scheduledTime}`)
+        //start: new Date(event.date, event.scheduledTime)
+        start: new Date(`${event.date} ${event.scheduledTime}`),
+      }));
     }
   },
   methods: {
     async addEvent() {
       if (this.newEventTitle && this.newEventDate) {
         const event = {
-          eventName: this.newEventTitle,
-          start: new Date(this.newEventDate)
-        }
+          title: this.newEventTitle,
+          //It likes this & this works
+          start: new Date(this.newEventDate + 'T' + this.newEventTime),
+        };
 
         if (this.newEventDesc) {
-          event.description = this.newEventDesc
+          event.description = this.newEventDesc;
         }
 
         // Add the new event to the FullCalendar
-        this.$refs.calendar.getApi().addEvent(event)
+        this.$refs.calendar.getApi().addEvent(event);
 
         await this.insertData({
           date: this.newEventDate,
           eventName: this.newEventTitle,
-          description: this.newEventDesc
-        })
+          scheduledTime: this.newEventTime,
+          description: this.newEventDesc,
+        });
 
         // Clear input fields
-        this.newEventTitle = ''
-        this.newEventDate = ''
-        this.newEventDesc = ''
+        this.newEventTitle = '';
+        this.newEventDate = '';
+        this.newEventTime = '';
+        this.newEventDesc = '';
       } else {
-        alert('Please enter event title and date')
+        alert('Please enter event title and date');
       }
     },
     async insertData(data) {
-      const { data: insertedData, error } = await supabase.from('Events').insert([data])
+      const { data: insertedData, error } = await supabase
+        .from('Events')
+        .insert([data]);
       if (error) {
         console.error(error)
         this.$root.snackbar.show({ text: 'Error check log', timeout: 10000, color: 'red' })
       } else {
-        console.log('Data inserted:', insertedData)
-        this.$root.snackbar.show({
-          text: 'Data inserted into table',
-          timeout: 10000,
-          color: 'green'
-        })
+        console.log('Data inserted:', insertedData);
+        this.$root.snackbar.show({ text: 'Data inserted into table', timeout: 10000, color: 'green' })
       }
-    }
+    },
   }
-}
+};
 </script>
+
