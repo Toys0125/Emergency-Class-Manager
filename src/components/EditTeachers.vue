b6b9613f6bfd800405f8db0e68216a6787132da1
toys0125 <marcus.g.stange@gmail.com>
2023-12-08 18:27:06 -0600

    Removed userdata Fetch with global.

--- src/components/EditTeachers.vue
+++ src/components/EditTeachers.vue
@@ -179,33 +179,34 @@ export default {
     tab: 1
   }),
   methods: {
-    async fetchUserData() {
+    /*async fetchUserData() {
       try {
-        const { data: { user } } = await supabase.auth.getUser();
+        const {
+          data: { user }
+        } = await supabase.auth.getUser()
 
         if (user && user.email) {
-          const userEmail = user.email;
+          const userEmail = user.email
 
           const { data: userData, error: userError } = await supabase
             .from('Users')
             .select('school_id')
             .eq('userEmail', userEmail)
-            .single();
+            .single()
 
-            console.log("school id: " + userData.school_id)
+          console.log('school id: ' + userData.school_id)
           if (userError) {
-            console.error('Error fetching user data:', userError);
+            console.error('Error fetching user data:', userError)
           } else {
-            
-            this.school_id = userData.school_id; // Set the school_id property
+            this.school_id = userData.school_id // Set the school_id property
           }
         } else {
-          console.error('User email not found.');
+          console.error('User email not found.')
         }
       } catch (error) {
-        console.error('Error fetching user data:', error);
+        console.error('Error fetching user data:', error)
       }
-    },
+    },*/
     loadRows({ page, itemsPerPage, sortBy }) {
       this.loading = true
       console.log(this.modalData.class_id)
@@ -406,7 +407,8 @@ export default {
   async mounted() {
     this.modalData = this.passedData
     console.log('Mounted', this.modalData)
-    await this.fetchUserData();
+    var userData = await this.$root.userData
+    this.school_id = userData.school_id;
   },
   beforeRouteLeave(to, from, next) {
     if (this.addedRows.length > 0) {
