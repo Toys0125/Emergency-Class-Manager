2a0605f2c1ca2967fbb3c260cdbf026caad8d0cb
khoskins2 <hoskinskatie1@gmail.com>
2023-12-14 00:23:38 -0600

    fix build

--- src/views/Base Page/Home.vue
+++ src/views/Base Page/Home.vue
@@ -5,7 +5,7 @@
         <!-- Adjust the column width to take the full width of the container -->
         <v-card class="text-center" style="width: 100%;">
           <v-img
-            src="@/assets/Emergency Class Manager Logo.svg"
+            src="@/assets/Emergency%20Class%20Manager%20Logo.svg"
             alt="Emergency Image"
             class="my-3 smaller-image"
           ></v-img>
